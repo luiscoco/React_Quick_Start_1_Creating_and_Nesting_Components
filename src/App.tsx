27929016f9ac2b3a870a7fb23144d8a806012e57
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2025-10-23 22:58:06 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <MyButton />
+    </div>
+  );
+}
